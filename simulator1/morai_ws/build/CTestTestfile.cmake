bf2871fbf9f1e699da694e82f7af1d0b7458641f
Jang Hong Joon <wkdghdwns19969@gmail.com>
2025-09-08 16:54:43 +0900

    chore : 디렉토리 구조 정리

--- simulator1/morai_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/S13P21A507/simulator1/morai_ws/src
-# Build directory: /home/ubuntu/S13P21A507/simulator1/morai_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("morai_msgs")
--- simulator1/morai_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ubuntu/S13P21A507/simulator1/morai_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ubuntu/morai_ws/devel;/opt/ros/noetic'.split(';') if '/home/ubuntu/morai_ws/devel;/opt/ros/noetic' != '' else []
