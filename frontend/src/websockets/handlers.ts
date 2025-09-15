013c05beb5896e19d1c27deca4f290f4dc902eed
craftor-3622 <wnsjh9110@naver.com>
2025-09-15 14:09:24 +0900

    fix(fe): handler 코드 수정

--- frontend/src/websockets/handlers.ts
+++ frontend/src/websockets/handlers.ts
@@ -33,11 +33,12 @@ export function onVehicle(binaryData: ArrayBuffer) {
     // 차량 위치 정보 업데이트
     case 0x11: {
       const num_of_vehicle = view.getUint32(1, true);
+      console.log(num_of_vehicle);
 
       for (let i = 0; i < num_of_vehicle; i++) {
-        const vehicle_id = view.getUint32(5 + 4 * i, true);
-        const posX = view.getFloat32(9 + 4 * i, true);
-        const posY = view.getFloat32(13 + 4 * i, true);
+        const vehicle_id = view.getUint32(5 + 12 * i, true);
+        const posX = view.getFloat32(9 + 12 * i, true);
+        const posY = view.getFloat32(13 + 12 * i, true);
         console.log(vehicle_id, posX, posY);
       }
 
