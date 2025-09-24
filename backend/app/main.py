b8001c2ab565fd2b6e00d2ecf44ff8d4c00ef20a
홍창근 <hong9640@naver.com>
2025-09-24 09:04:25 +0900

    docs: 프론트엔드 디버깅 코드 삭제, main.py 일부 수정

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,12 +41,10 @@ async def lifespan(app: FastAPI):
     print("--- FastAPI app shutdown. ---")
 
 # FastAPI 앱 인스턴스 생성
-app = FastAPI(lifespan=lifespan)
 app = FastAPI(
+    lifespan=lifespan,
     docs_url="/api/docs",
-    redoc_url="/api/redoc",
-    openapi_url="/api/openapi.json"
-)
+    )
 
 # --- 전역 예외 핸들러 --- 
 @app.exception_handler(APIException)
--- frontend/src/Apis.ts
+++ frontend/src/Apis.ts
@@ -3,7 +3,6 @@ const BASE_API_URL = import.meta.env.VITE_API_BASE;
 
 // 차량 리스트 조회
 export const getVehicleListAPI = async () => {
-  console.log("API 요청 직전 BASE_API_URL 값:", BASE_API_URL);
   try {
     const response = await axios.get(`${BASE_API_URL}/vehicles`);
     return response.data.vehicles;
@@ -14,7 +13,6 @@ export const getVehicleListAPI = async () => {
 
 // 단일 차량 조회
 export const getVehicleAPI = async (vehicle_id: number) => {
-  console.log("API 요청 직전 BASE_API_URL 값:", BASE_API_URL);
   try {
     const response = await axios.get(`${BASE_API_URL}/vehicles/${vehicle_id}`);
     return response.data;
@@ -25,7 +23,6 @@ export const getVehicleAPI = async (vehicle_id: number) => {
 
 // 단일 차량 조회
 export const deleteVehicleAPI = async (vehicle_id: number) => {
-  console.log("API 요청 직전 BASE_API_URL 값:", BASE_API_URL);
   try {
     const response = await axios.delete(`${BASE_API_URL}/vehicles/${vehicle_id}`);
     return response;
@@ -36,7 +33,6 @@ export const deleteVehicleAPI = async (vehicle_id: number) => {
 
 // 사건 로그 조회
 export const getEventListAPI = async () => {
-  console.log("API 요청 직전 BASE_API_URL 값:", BASE_API_URL);
   try {
     const response = await axios.get(`${BASE_API_URL}/vehicles/events`);
     return response.data.events;
@@ -47,7 +43,6 @@ export const getEventListAPI = async () => {
 
 // 단일 지도 (GeoJson) 조회
 export const getMapAPI = async (map_id: number) => {
-  console.log("API 요청 직전 BASE_API_URL 값:", BASE_API_URL);
   try {
     const response = await axios.get(`${BASE_API_URL}/maps/${map_id}`, {
       headers: { Accept: "application/geo+json, application/json" },
